50a71f1cacb56ae47bde5a58f36be02aa3a0d49a
thefr3spirit <beofr3spirit@gmail.com>
2026-05-31 14:40:19 +0300

    fix: update Firebase to 4.x to resolve CocoaPods GoogleUtilities conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart' hide FirebaseService;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
